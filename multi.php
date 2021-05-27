4f0d5d357a1cd93ab3c6c260863f1cee662efa0e
NAYI BHAVISHA AMRISHBHAI <nayibhavi44@gmail.com>
2021-05-27 16:54:47 +0530

    v1

---
+++ multi.php
@@ -0,0 +1,13 @@
+<?php 
+
+$a ='Bhavisha';
+	echo "<table border=1px solid>";
+	
+for ($i=1; $i <= 100 ; $i++) { 
+	echo "<tr>";
+	echo "<td><br/>$i :hello $a!</td>";
+	echo "</tr>";
+}
+
+echo "</table>";
+?>
